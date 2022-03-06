9599feffc217e4e2794c30c82a683284b0f2765f
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-06 11:21:19 +0800

    demo01 测试方法

--- MyBatis_demo01/src/test/java/com/litway/mapper/UserMapperTest.java
+++ MyBatis_demo01/src/test/java/com/litway/mapper/UserMapperTest.java
@@ -1,5 +1,6 @@
 package com.litway.mapper;
 
+import com.litway.entity.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -8,10 +9,52 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class UserMapperTest {
+
+    @Test
+    public void findAll() throws IOException {
+        // 读取mybatis核心配置文件
+        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+        // 新建一个sqlSession工厂实体
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        // 调用工厂获取一个sqlSession
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+        // 通过工厂来获取连接 -> 自动提交
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+
+        // 获取接口对象 -> 反射!
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        List<User> all = mapper.findAll();
+        for (User user : all) {
+            System.out.println(user);
+        }
+
+        // 关闭连接
+        sqlSession.close();
+    }
+
+    @Test
+    public void getUserById() throws IOException {
+        // 读取mybatis核心配置文件
+        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+        // 新建一个sqlSession工厂实体
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        // 调用工厂获取一个sqlSession
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+        // 通过工厂来获取连接 -> 自动提交
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+
+        // 获取接口对象 -> 反射!
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        System.out.println(mapper.getUserById());
+
+        // 关闭连接
+        sqlSession.close();
+    }
 
     @Test
     public void deleteUser() throws IOException {
