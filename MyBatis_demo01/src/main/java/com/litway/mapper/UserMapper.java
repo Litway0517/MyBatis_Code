f765fd138e139980b4fb11ab45100e7ecefb3bd0
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-06 11:07:02 +0800

    demo01 增加删除方法 和 更新方法

--- MyBatis_demo01/src/main/java/com/litway/mapper/UserMapper.java
+++ MyBatis_demo01/src/main/java/com/litway/mapper/UserMapper.java
@@ -1,8 +1,23 @@
 package com.litway.mapper;
 
+/**
+ * 用户映射器
+ *
+ * @author DELL_
+ * @date 2022/03/06
+ */
 public interface UserMapper {
 
+    /**
+     * 插入用户
+     *
+     * @return int
+     */
     public int insertUser();
+
+    public int updateUser();
+
+    public int deleteUser();
 
 
 
