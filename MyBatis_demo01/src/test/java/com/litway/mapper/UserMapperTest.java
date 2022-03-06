345687ec1e541a32f59934ffcfbb720668abaa8f
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-06 11:07:13 +0800

    demo01 测试更新方法和删除方法

--- MyBatis_demo01/src/test/java/com/litway/mapper/UserMapperTest.java
+++ MyBatis_demo01/src/test/java/com/litway/mapper/UserMapperTest.java
@@ -14,6 +14,44 @@ import static org.junit.Assert.*;
 public class UserMapperTest {
 
     @Test
+    public void deleteUser() throws IOException {
+        // 读取mybatis核心配置文件
+        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+        // 新建一个sqlSession工厂实体
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        // 调用工厂获取一个sqlSession
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+        // 通过工厂来获取连接 -> 自动提交
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+
+        // 获取接口对象 -> 反射!
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        System.out.println(mapper.deleteUser());
+
+        // 关闭连接
+        sqlSession.close();
+    }
+
+    @Test
+    public void updateUser() throws IOException {
+        // 读取mybatis核心配置文件
+        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+        // 新建一个sqlSession工厂实体
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        // 调用工厂获取一个sqlSession
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+        // 通过工厂来获取连接 -> 自动提交
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+
+        // 获取接口对象 -> 反射!
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        System.out.println(mapper.updateUser());
+
+        // 关闭连接
+        sqlSession.close();
+    }
+
+    @Test
     public void insertUser() throws IOException {
         // 读取mybatis核心配置文件
         InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
