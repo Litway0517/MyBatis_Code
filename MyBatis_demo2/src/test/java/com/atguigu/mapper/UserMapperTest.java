1623e7727828344f1b48c9c6257b37e4cbabe824
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-07 22:51:35 +0800

    demo2 测试

---
+++ MyBatis_demo2/src/test/java/com/atguigu/mapper/UserMapperTest.java
@@ -0,0 +1,37 @@
+package com.atguigu.mapper;
+
+import com.atguigu.entity.User;
+import com.atguigu.utils.SqlSessionUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class UserMapperTest {
+
+    @Test
+    public void test() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        assert sqlSession != null;
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        // select * from `t_user` where `username` = ? (BaseJdbcLogger.java:137)
+        User litway = mapper.getUserByUsername("张三");
+        System.out.println(litway);
+
+        User user = mapper.getUserByUsername2("张三");
+        // select * from `t_user` where `username` = '张三' (BaseJdbcLogger.java:137)
+        System.out.println(user);
+
+    }
+
+    @Test
+    public void findAll() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        assert sqlSession != null;
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        List<User> all = mapper.findAll();
+        all.forEach(System.out::println);
+    }
+}
