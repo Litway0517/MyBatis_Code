24adc7a17053615d27710ac1449c6eeda59cec1c
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-08 22:53:02 +0800

    demo2 模糊查询SQL '%#{参数}%' 此时占位符会被解析为字符串 所以不能够正确赋值 导致查询错误. 有三种解决方式: ${} concat函数 直接使用双字符串拼接%

---
+++ MyBatis_demo2/src/main/java/com/atguigu/mapper/SQLMapper.java
@@ -0,0 +1,14 @@
+package com.atguigu.mapper;
+
+import com.atguigu.entity.User;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface SQLMapper {
+
+    // 根据用户名进行 模糊查询
+    List<User> getUserByLike(@Param("username") String username);
+
+
+}
