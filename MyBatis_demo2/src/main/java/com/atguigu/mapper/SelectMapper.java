b5eb33bc5b1fec2fb5f6b0cd45c1189c608b43c7
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-08 16:42:09 +0800

    demo2 查询的时候要确定好返回的结果是什么 一定要注意

--- MyBatis_demo2/src/main/java/com/atguigu/mapper/SelectMapper.java
+++ MyBatis_demo2/src/main/java/com/atguigu/mapper/SelectMapper.java
@@ -3,10 +3,15 @@ package com.atguigu.mapper;
 import com.atguigu.entity.User;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 public interface SelectMapper {
 
     // 根据id查询用户信息
     public User getUserById(@Param("userId") Integer id);
+
+    // 查询所有用户
+    public List<User> findAllUser();
 
 
 }
