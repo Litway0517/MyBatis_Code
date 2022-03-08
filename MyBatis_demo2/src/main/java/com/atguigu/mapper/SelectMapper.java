eaeb913509a68e624e8d5e8bfef68d56762c5225
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-08 22:36:24 +0800

    demo2 @MapKey("id")注解的使用: 查询出来的结果为 -> SQL中的id列作为键 此id对应的数据为值 存储到Map集合中. id当然能改成其他列 但是一定要是唯一的 否则查询出来的数据会覆盖.

--- MyBatis_demo2/src/main/java/com/atguigu/mapper/SelectMapper.java
+++ MyBatis_demo2/src/main/java/com/atguigu/mapper/SelectMapper.java
@@ -1,6 +1,7 @@
 package com.atguigu.mapper;
 
 import com.atguigu.entity.User;
+import org.apache.ibatis.annotations.MapKey;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -19,6 +20,20 @@ public interface SelectMapper {
 
     // 根据id查询用户 -> 返回为Map集合(如果有时候查询出来的结果没有一个实体能够接收, 那么就可以用Map对象接收)
     Map<String, Object> getUserByIdToMap(@Param("userId") Integer userId);
+
+    // 第一种方式: 查询所有数据 -> 存储到Map类型的List集合来接收
+    List<Map<String, Object>> getAllUserToMap();
+
+    /*
+        第二种方式: 查询所有的user用户. 需要使用@MapKey()注解
+        @MapKey("查询结果列中的某一个字段的名字")
+        MapKey("id"): 是指使用查询出来的结果字段中的id作为键, 然后对应该id的整条结果作为值存储到Map中.
+        因此这个字段一定要保证唯一. 基本上就是主键. 如果不是唯一的结果, 那么会形成覆盖而不会报错.
+        Map<注解值, 查询出来的结果>
+     */
+    @MapKey("id")
+    // @MapKey("username")
+    Map<String, Object> getAllUserToMapByMapkey();
 
 
 }
