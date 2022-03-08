e9453c2ee9494194cc09b5adb3c03b3c4a0747a0
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-08 22:15:34 +0800

    demo2 mapper接口的方法返回值为Map对象. 此时以字段为键 查询字段的结果为值 存储到Map中

--- MyBatis_demo2/src/main/java/com/atguigu/mapper/SelectMapper.java
+++ MyBatis_demo2/src/main/java/com/atguigu/mapper/SelectMapper.java
@@ -4,6 +4,7 @@ import com.atguigu.entity.User;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 public interface SelectMapper {
 
@@ -15,6 +16,9 @@ public interface SelectMapper {
 
     // 查询特殊字段 -> user表的总记录数
     Integer getCount();
+
+    // 根据id查询用户 -> 返回为Map集合(如果有时候查询出来的结果没有一个实体能够接收, 那么就可以用Map对象接收)
+    Map<String, Object> getUserByIdToMap(@Param("userId") Integer userId);
 
 
 }
