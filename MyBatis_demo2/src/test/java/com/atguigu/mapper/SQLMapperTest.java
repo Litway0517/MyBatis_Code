753452243b614aed9d87d6cad193883fea590c96
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-09 15:21:54 +0800

    demo2 测试

--- MyBatis_demo2/src/test/java/com/atguigu/mapper/SQLMapperTest.java
+++ MyBatis_demo2/src/test/java/com/atguigu/mapper/SQLMapperTest.java
@@ -12,6 +12,24 @@ import static org.junit.Assert.*;
 public class SQLMapperTest {
 
     @Test
+    public void getUserByTableName() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        assert sqlSession != null;
+        SQLMapper sqlMapper = sqlSession.getMapper(SQLMapper.class);
+        List<User> t_user = sqlMapper.getUserByTableName("t_user");
+        System.out.println(t_user);
+    }
+
+    @Test
+    public void deleteMore() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        assert sqlSession != null;
+        SQLMapper sqlMapper = sqlSession.getMapper(SQLMapper.class);
+        int i = sqlMapper.deleteMore("1, 2, 3");
+        System.out.println(i);
+    }
+
+    @Test
     public void getUserByLike() {
         SqlSession sqlSession = SqlSessionUtils.getSqlSession();
         assert sqlSession != null;
