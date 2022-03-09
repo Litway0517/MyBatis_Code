7f7035a694a972c1aad50de1ef4f7842e1a3c97a
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-09 22:31:41 +0800

    demo3 增加查询所有员工方法

--- MyBatis_demo3/src/main/java/com/atguigu/mapper/EmployeeMapper.java
+++ MyBatis_demo3/src/main/java/com/atguigu/mapper/EmployeeMapper.java
@@ -1,8 +1,13 @@
 package com.atguigu.mapper;
 
+import com.atguigu.domain.Employee;
+
+import java.util.List;
+
 public interface EmployeeMapper {
 
-
+    // 查询所有员工
+    List<Employee> getAllEmployee();
 
 
 
