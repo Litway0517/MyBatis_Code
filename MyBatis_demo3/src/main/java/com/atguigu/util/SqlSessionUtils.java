d4ae52a8c65d00ba25216e2e09dd773b95097592
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-09 22:31:22 +0800

    demo3 工具类

---
+++ MyBatis_demo3/src/main/java/com/atguigu/util/SqlSessionUtils.java
@@ -0,0 +1,27 @@
+package com.atguigu.util;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class SqlSessionUtils {
+
+    public static SqlSession getSqlSession() {
+        InputStream resourceAsStream = null;
+        try {
+            resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+            return sqlSessionFactory.openSession(true);
+        } catch (IOException e) {
+            System.out.println("获取 MyBatis 核心配置文件失败 -> 请检查 ");
+            return null;
+        }
+    }
+
+
+
+}
