fbded169b1e13f3773d6f224fb074e4a449be690
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-11 22:34:51 +0800

    demo3 动态SQL中的if标签

--- MyBatis_demo3/src/main/java/com/atguigu/mapper/DynamicSQL.java
+++ MyBatis_demo3/src/main/java/com/atguigu/mapper/DynamicSQL.java
@@ -1,8 +1,13 @@
 package com.atguigu.mapper;
 
+import com.atguigu.domain.Employee;
+
+import java.util.List;
+
 public interface DynamicSQL {
 
-
+    // 多条件查询
+    List<Employee> getEmployeeByCondition(Employee employee);
 
 
 
