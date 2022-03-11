cd1d9c3ac373b70c70896279948e474e9edb50cc
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-11 11:45:55 +0800

    demo3 第二步查询员工信息 根据第一步的结果查询出来其部门信息

--- MyBatis_demo3/src/main/java/com/atguigu/mapper/DeptMapper.java
+++ MyBatis_demo3/src/main/java/com/atguigu/mapper/DeptMapper.java
@@ -1,4 +1,12 @@
 package com.atguigu.mapper;
 
+import com.atguigu.domain.Dept;
+import org.apache.ibatis.annotations.Param;
+
 public interface DeptMapper {
+
+    /*
+        分步查询第二步
+     */
+    Dept getEmpAndDeptByStepTwo(@Param("dId") Integer dId);
 }
