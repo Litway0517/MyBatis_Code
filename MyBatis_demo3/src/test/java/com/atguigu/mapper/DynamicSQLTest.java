1c8b81737c493051857fa2c7d0b0178a3f4af565
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-11 22:35:44 +0800

    demo3 测试

---
+++ MyBatis_demo3/src/test/java/com/atguigu/mapper/DynamicSQLTest.java
@@ -0,0 +1,24 @@
+package com.atguigu.mapper;
+
+import com.atguigu.domain.Employee;
+import com.atguigu.util.SqlSessionUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class DynamicSQLTest {
+
+    @Test
+    public void getEmployeeByCondition() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        assert sqlSession != null;
+        DynamicSQL dynamicSQL = sqlSession.getMapper(DynamicSQL.class);
+        Employee employee = new Employee(null, "张三", 25, "", "");
+        List<Employee> employeeList = dynamicSQL.getEmployeeByCondition(employee);
+        System.out.println(employeeList);
+
+    }
+}
