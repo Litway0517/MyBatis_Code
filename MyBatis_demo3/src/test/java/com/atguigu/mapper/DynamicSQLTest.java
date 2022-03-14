950368aa0154d833662471aeaf953c1f0b8c33c5
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-14 11:23:33 +0800

    demo3 测试

--- MyBatis_demo3/src/test/java/com/atguigu/mapper/DynamicSQLTest.java
+++ MyBatis_demo3/src/test/java/com/atguigu/mapper/DynamicSQLTest.java
@@ -5,11 +5,26 @@ import com.atguigu.util.SqlSessionUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class DynamicSQLTest {
+
+    @Test
+    public void insertEmployees() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        assert sqlSession != null;
+        DynamicSQL dynamicSQL = sqlSession.getMapper(DynamicSQL.class);
+        Employee employee1 = new Employee(null, "a1", 23, "男", "a1@qq.com");
+        Employee employee2 = new Employee(null, "a2", 24, "男", "a1@qq.com");
+        Employee employee3 = new Employee(null, "a3", 25, "男", "a1@qq.com");
+        List<Employee> employees = Arrays.asList(employee1, employee2, employee3);
+        int i = dynamicSQL.insertEmployees(employees);
+        System.out.println(i);
+
+    }
 
     @Test
     public void deleteEmployeeByIdsTwo() {
